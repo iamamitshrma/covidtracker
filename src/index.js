e7f9424e8da2ea0e6d21e8b9c66136ffef5142d0
Amit Sharma <amitsharma199938@gmail.com>
2021-06-01 18:32:34 +0530

    update success

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Covid from './components/Covid'
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
